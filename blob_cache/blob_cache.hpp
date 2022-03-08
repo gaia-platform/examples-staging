396cd68ff6f605d910c33b94448d7f5a3be84d84
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2022-03-08 10:53:50 -0800

    Refactor implementation

--- blob_cache/blob_cache.hpp
+++ blob_cache/blob_cache.hpp
@@ -3,10 +3,11 @@
 // All rights reserved.
 /////////////////////////////////////////////
 
-#include <stddef.h>
-#include <stdint.h>
+#include <cstddef>
+#include <cstdint>
 
-#include <map>
+#include <unordered_map>
+#include <shared_mutex>
 
 // A mechanism for storing non-persistent memory blobs.
 // ​
@@ -34,12 +35,12 @@
 // ID. If blobs are created and destroyed, there must be a safe way to
 // delete old ones, and this must operate in a world where a delete call
 // may be issued many times (e.g., if called repeatedly during retried
-// transactions) and should be tolerate of a delete being ignored (e.g.,
+// transactions) and should be tolerant of a delete being ignored (e.g.,
 // failed transaction that's not retried). This is achieved by each blob
-// having a reference to a blob that it supercedes, so if A is allocated
-// and B is meant to supercede A (e.g., because of a resize), then a pointer
-// to A is stored within B. Even though A is superceded, it remains a
-// valid memory allocation until B is superceded. Only when B is superceded
+// having a reference to a blob that it supersedes, so if A is allocated
+// and B is meant to supersede A (e.g., because of a resize), then a pointer
+// to A is stored within B. Even though A is superseded, it remains a
+// valid memory allocation until B is superseded. Only when B is superseded
 // will A be deleted. If B is allocated but the transaction is rolled back
 // and not retried, then A will remain in use. B will be allocated but
 // won't be used until the calling process asks the blob factory for a
@@ -52,50 +53,74 @@
 // newly created blob will have been allocated but the ID will not be
 // incremented, so if it's retried it will request a blob with the same ID
 // again. That request can simultaneously have the former ID be
-// superceded. E.g.,:
+// superseded. E.g.,:
 //   blob_t* blob = blob_factory_t.create_blob(
 //       ID+1,   // new blob ID
 //       size_in_bytes,  // size of memory to be allocated
 //       ID      // superceded blob
 //   );
 
-class blob_t
+constexpr uint32_t c_invalid_blob_id = 0;
+
+enum class blob_state_t : uint8_t
+{
+    not_set = 0,
+
+    initialized = 1,
+    used = 2,
+};
+
+struct blob_t
 {
 public:
-    static constexpr uint32_t c_invalid_blob_id = 0;
-
-    // Creates a blob with the specified ID and characteristics
-    static blob_t* create_blob(uint32_t id, size_t size, uint32_t id_superseded_blob = c_invalid_blob_id);
-
-    // Retrieves a blob given an id.
-    // Will return nullptr if the blob doesn't exist.
-    static blob_t* get_blob(uint32_t id);
-
-public:
-    // states are probably NEWLY_CREATED, VALID, and DIRTY
-    uint32_t get_state() { return m_state; }
-    void set_state(uint32_t new_state) { m_state = new_state;  }
-
-    uint8_t* data() { return m_data; }
-    size_t size() { return m_size; }
-
-protected:
     blob_t(uint32_t id, size_t size, uint32_t id_superseded_blob);
     ~blob_t();
 
-protected:
-    uint32_t m_id;
-    size_t m_size;
-    // arbitrary blob content -- m_data = malloc(m_size)
-    uint8_t* m_data;
+    // Deallocate blob memory.
+    void clear();
 
-    uint32_t m_state;
+    // Reset blob content.
+    void reset(uint32_t id, size_t size, uint32_t id_superseded_blob);
+
+public:
+    uint32_t id;
+
+    // Blob data information.
+    size_t size;
+    uint8_t* data;
+
+    // Blob state.
+    blob_state_t state;
 
     // Blob that this blob supersedes.
     // It will be deleted when this blob itself is superseded.
-    uint32_t m_id_superseded_blob;
+    uint32_t id_superseded_blob;
+};
+
+class blob_cache_t
+{
+public:
+    static blob_cache_t* get();
+
+public:
+    // Creates a blob with the specified ID and characteristics
+    blob_t* create_or_reset_blob(uint32_t id, size_t size, uint32_t id_superseded_blob = c_invalid_blob_id);
+
+    // Retrieves a blob given an id.
+    // Will return nullptr if the blob doesn't exist.
+    blob_t* get_blob(uint32_t id);
+
+protected:
+    blob_cache_t() = default;
+
+    blob_cache_t(const blob_cache_t&) = delete;
+    blob_cache_t& operator=(const blob_cache_t&) = delete;
 
 protected:
     // Map of allocated blobs.
-    static std::map<uint32_t, blob_t*> s_blob_map;
+    std::unordered_map<uint32_t, blob_t*> m_blob_map;
+    std::shared_mutex m_lock;
+
+protected:
+    static blob_cache_t s_blob_cache;
 };
