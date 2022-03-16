f40653984c0b315d2e4a7350229f808c94da33e3
Simone Rondelli <simone@gaiaplatform.io>
2022-03-16 19:05:30 +0100

    Fix the example

--- pick_and_place/src/main.cpp
+++ pick_and_place/src/main.cpp
@@ -12,15 +12,49 @@
 
 using namespace gaia::pick_and_place;
 
+/**
+ * Remove all the objects for the given type T_type.
+ */
+template <typename T_type>
+void clear_table()
+{
+    for (auto obj_it = T_type::list().begin(); obj_it != T_type::list().end();)
+    {
+        auto current_obj_it = obj_it++;
+        current_obj_it->delete_row();
+    }
+}
+
+/**
+ * Remove all rows in the database.
+ */
+void clear_data()
+{
+    clear_table<state_t>();
+    clear_table<move_action_t>();
+    clear_table<pick_action_t>();
+}
+
 int main()
 {
     gaia::system::initialize();
 
+    // Start clean.
     gaia::db::begin_transaction();
-    state_writer state_w;
+    clear_data();
+    gaia::db::commit_transaction();
+
+    // Initial state, should not trigger any rule.
+    gaia::db::begin_transaction();
+    state_t state = state_t::get(state_t::insert_row(true, true));
+    gaia::db::commit_transaction();
+
+    // Update the state to [pick && ~place] should trigger a rule.
+    gaia::db::begin_transaction();
+    state_writer state_w = state.writer();
     state_w.pick = true;
     state_w.place = false;
-    state_w.insert_row();
+    state_w.update_row();
     gaia::db::commit_transaction();
 
     gaia::system::shutdown();
